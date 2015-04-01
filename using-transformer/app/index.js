c0e70c047edfdf7eaab9276df6119805fe3da170
Mantas Kunsmanas <mantas.kunsmanas@devbridge.com>
2015-04-01 23:08:25 +0300

    Run using JSXTransformer from CDN

---
+++ using-transformer/app/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var RootComponent = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+});
+
+React.render(<RootComponent />, document.body);
